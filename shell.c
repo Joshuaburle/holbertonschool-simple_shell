611fa6806319d37f6a3870695839823de17f4a11
Moha-A1 <11469@holbertonstudents.com>
2025-08-20 02:42:15 +0200

    correction task 3

--- shell.c
+++ shell.c
@@ -33,32 +33,7 @@ char *read_line(void)
 	return (line);
 }
 
-/**
- * split - Divise une ligne en un seul argument (pour la tâche 0.1)
- * @line: La ligne à analyser
- * Return: Tableau avec un seul argument, ou NULL en cas d'erreur
- */
-char **split(char *line)
-{
-	char **args = malloc(2 * sizeof(char *));
-	char *token;
 
-	if (args == NULL)
-		return (NULL);
-
-	/* Prendre seulement le premier mot (pas d'arguments) */
-	token = strtok(line, " \t\r\n");
-	if (token == NULL)
-	{
-		free(args);
-		return (NULL);
-	}
-
-	args[0] = token;
-	args[1] = NULL;
-
-	return (args);
-}
 
 /**
  * execute_command - Exécute une commande (un seul mot, pas d'arguments)
