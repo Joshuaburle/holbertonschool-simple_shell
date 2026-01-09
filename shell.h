a02951745a9f0f03e3541e0f73df69670b6e1bbf
Joshuaburle <11491@holbertonschool.com>
2026-01-09 14:13:01 +0100

    Add builtins.c for env builtin and update shell.c to handle builtins

--- shell.h
+++ shell.h
@@ -18,17 +18,19 @@ void free_args(char **argv);
 /* execution */
 int execute_command(char **argv, char *av0, unsigned int count);
 
+/* path */
+char *resolve_cmd(char *cmd, char *av0, unsigned int count, int *err_status);
+
 /* errors */
 void print_not_found(char *av0, unsigned int count, char *cmd);
 void print_perm_denied(char *av0, unsigned int count, char *cmd);
 
-/* PATH */
-char *resolve_cmd(char *cmd, char *av0, unsigned int count, int *err_status);
-
-/* exit */
+/* builtins */
 int handle_exit(char **argv, char *line, int last_status);
+int handle_env(char **argv);
+
+/* loop */
 int process_line(char *line, char **av, unsigned int *count, int *last_status);
 int shell_loop(char **av);
-
 
 #endif /* SHELL_H */
